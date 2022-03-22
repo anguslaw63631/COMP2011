dc01d923073ecc3afc2dea9f19028366220a4bbf
anguslaw63631 <anguslaw63631@gmail.com>
2022-03-22 21:47:40 +0800

    Pa2 done task5

--- Pa2/pa2.cpp
+++ Pa2/pa2.cpp
@@ -50,12 +50,12 @@ void printString(const char str[])
         printString(str + 1);
     }
 }
-float power(float x, int y)
+float pow(float x, int y)
 {
     float temp;
     if (y == 0)
         return 1;
-    temp = power(x, y / 2);
+    temp = pow(x, y / 2);
     if (y % 2 == 0)
         return temp * temp;
     else
@@ -71,13 +71,112 @@ int getIntValue(const char str[], int start, int end)
 {
     if ((end - start) <= 0)
         return str[start] - 48;
-    return (getIntValue(str, start + 1, end) + (str[start] - 48) * (power(10, end - start)));
+    return (getIntValue(str, start + 1, end) + (str[start] - 48) * (pow(10, end - start)));
+}
+
+int strlen(const char str[])
+{
+    if (*str == 0)
+    {
+        return 0;
+    }
+    return strlen(str + 1) + 1;
+}
+int findNext(const char str[], int lens, int start)
+{
+    if (start >= lens)
+    {
+        return -1;
+    }
+    if (str[start] >= '0' && str[start] <= '9')
+    {
+        return findNext(str, lens, start + 1);
+    }
+    else
+    {
+        // cout << str[start] << ":"<<start << endl ;
+        return start;
+    }
+}
+
+int findBack(const char str[], int lens, int start)
+{
+    if (start < 0)
+    {
+        return -1;
+    }
+    if (str[start] >= '0' && str[start] <= '9')
+    {
+        return findBack(str, lens, start - 1);
+    }
+    else
+    {
+        // cout << str[start] << ":"<<start << endl ;
+        return start;
+    }
+}
+
+int evaluate(const char str[], int start, int end, int lens)
+{
+    if (end - start < 0)
+    {
+        return 0;
+    }
+
+    int next = findNext(str, lens, start);
+    //cout << "Begin: " << start << " end: " << end << " next: " << next << endl;
+    if (next == -1)
+    {
+        if(str[findBack(str,lens,start)]== '-'){
+            return -getIntValue(str, start, end);
+        }
+        return getIntValue(str, start, end);
+    }
+    else if (next != start)
+    {
+
+        //cout << "IF | start: " << start << " end: " << end << " next: " << next << endl;
+        //cout << "get Value: " << getIntValue(str, start, next - 1)<<endl;
+        if(str[findBack(str,lens,start)]== '-'){
+            return evaluate(str, start + (next-start+1) , end, lens) - getIntValue(str, start, next - 1);
+        }
+        return evaluate(str, start + (next-start+1) , end, lens) + getIntValue(str, start, next - 1);
+    }
+    else if (next == 0)
+    {
+        int next2 = findNext(str, lens, next + 1);
+        int temp2;
+
+        if (next2 != -1)
+        {
+            temp2 = next2;
+            //cout << "temp2:" << temp2 << endl;
+        }
+        else
+        {
+            temp2 = end;
+        }
+
+        if (str[next] == '+')
+        {
+            //cout << "IF Add 2| start: " << start << " temp2: " << temp2 << " next: " << next << endl;
+            return evaluate(str, temp2 + 1, end, lens);
+        }
+        else if (str[next] == '-')
+        {
+            //cout << "IF Sub 2| start: " << start << " temp2: " << temp2 << " next: " << next << endl;
+            
+            return evaluate(str, temp2 + 1, end, lens) - getIntValue(str, next + 1, temp2);
+        }
+    }
 }
 
 int evaluate(const char str[])
 {
-    return 0; // change this line if you want
+    int lens = strlen(str);
+    return evaluate(str, 0, lens - 1, lens);
 }
+
 //***** Only write your code above this line, remember that NO loop is allowed! *****
 
 int main()
@@ -247,7 +346,7 @@ int main()
     else if (testCase == 17) // test evaluate
     {
         cout << evaluate("1+2-4") << endl;
-    }
+    }                        // 01234
     else if (testCase == 18) // test evaluate
     {
         cout << evaluate("11+21-43") << endl;
@@ -259,6 +358,11 @@ int main()
     else if (testCase == 20) // test evaluate
     {
         cout << evaluate("-21") << endl;
+    }
+    else if (testCase == 21)
+    {
+        cout << "Find Next :"<<findNext("-21-2", 5, 1) << endl;
+        cout << "Find Back :"<<findBack("-2-12", 5, 4) << endl;
     }
 
     cout << "===================================" << endl;
